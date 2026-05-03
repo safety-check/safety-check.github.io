a16f133f95fc15cef785404ebb5bbde27d6977bb
safety-check <ahato.diamond.1@gmail.com>
2026-05-03 11:20:50 +0900

    Create script.js

---
+++ check6389363926374838/script.js
@@ -0,0 +1,17 @@
+var data = JSON.parse(atob(location.href.slice(location.href.indexOf('?index=') + 7)));
+function red(e) {
+  if (e) {
+    document.querySelector('#ipa').value = e;
+    //document.querySelector('#use').value = e[1];
+  }
+  document.querySelector('form').submit();
+}
+onmessage = m => red(m.data);
+document.addEventListener('DOMContentLoaded', () => {
+  let ifr = document.querySelector('iframe');
+  ifr.onload = () => ifr.contentWindow.postMessage('', '*');
+  ifr.src = data[2];
+  document.querySelector('form').action = data[0];
+  document.querySelector('#act').value = data[1];
+  document.querySelector('#bro').value = navigator.userAgent;
+});
